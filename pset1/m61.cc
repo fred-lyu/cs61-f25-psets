0589d0cb039713e93a38909306c9206455b21700
fred <a11652@tvt.net.cn>
2025-12-25 16:16:07 +0800

    [Add] pset1: all tests passed

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -6,11 +6,26 @@
 #include <cinttypes>
 #include <cassert>
 #include <sys/mman.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+constexpr char CANARY = 0xAB;
+constexpr size_t CANARY_SIZE = sizeof(CANARY);
+struct m61_memory_meta {
+    char file[16];
+    int line;
+};
+constexpr size_t META_SIZE = sizeof(m61_memory_meta) + CANARY_SIZE;
+
+constexpr size_t max_align = alignof(std::max_align_t);
+#define ROUND_UP(pos) ((pos + max_align - 1) / max_align * max_align)
 
 
 struct m61_memory_buffer {
     char* buffer;
-    size_t pos = 0;
+    // size_t pos = 0;
+    bool inited = false;
     size_t size = 8 << 20; /* 8 MiB */
 
     m61_memory_buffer();
@@ -35,6 +50,55 @@ m61_memory_buffer::~m61_memory_buffer() {
 }
 
 
+static m61_statistics stats = {};
+static std::map<uintptr_t, size_t> map_alloced_size;
+static std::unordered_set<uintptr_t> unset_freed;
+static std::map<uintptr_t, size_t> map_freed_size;
+
+
+static void m61_stats_alloc(void* ptr, size_t sz, const char* file, int line)
+{
+    if (ptr)
+    {
+        stats.nactive += 1;
+        stats.ntotal += 1;
+        stats.active_size += sz;
+        stats.total_size += sz;
+        map_alloced_size.insert({(uintptr_t)ptr, sz + META_SIZE});
+        ((char*)ptr)[sz - 1 + CANARY_SIZE] = CANARY;
+        m61_memory_meta* meta = reinterpret_cast<m61_memory_meta*>((char*)ptr + sz + CANARY_SIZE);
+        strncpy(meta->file, file, sizeof(meta->file));
+        meta->file[sizeof(meta->file) - 1] = '\0';
+        meta->line = line;
+    }
+    else
+    {
+        stats.nfail += 1;
+        stats.fail_size += sz;
+    }
+}
+
+
+static void* m61_find_free_space(size_t sz) {
+    // do we have a freed allocation that will work?
+    for (auto& freed : map_freed_size) {
+        size_t aligned_size = freed.second;
+        if (aligned_size >= sz) {
+            void* ptr = (void*)freed.first;
+            size_t aligned_sz = ROUND_UP(sz);
+            if (aligned_size > aligned_sz)
+            {
+                // 说明还有剩余对齐内存，需要保留
+                map_freed_size.insert({freed.first + aligned_sz, aligned_size - aligned_sz});
+            }
+            map_freed_size.erase(freed.first);
+
+            return ptr;
+        }
+    }
+    // otherwise fail
+    return nullptr;
+}
 
 
 /// m61_malloc(sz, file, line)
@@ -44,19 +108,58 @@ m61_memory_buffer::~m61_memory_buffer() {
 ///    The allocation request was made at source code location `file`:`line`.
 
 void* m61_malloc(size_t sz, const char* file, int line) {
-    (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-    if (default_buffer.pos + sz > default_buffer.size) {
-        // Not enough space left in default buffer for allocation
-        return nullptr;
+    void* ptr = nullptr;
+    if (sz == 0)
+    {
+        return ptr;
     }
 
-    // Otherwise there is enough space; claim the next `sz` bytes
-    void* ptr = &default_buffer.buffer[default_buffer.pos];
-    default_buffer.pos += sz;
+    if (!default_buffer.inited)
+    {
+        map_alloced_size.clear();
+        map_freed_size.clear();
+        map_freed_size.insert({(uintptr_t)default_buffer.buffer, default_buffer.size});
+        default_buffer.inited = true;
+    }
+
+    if (sz <= SIZE_MAX - META_SIZE)
+    {
+        ptr = m61_find_free_space(sz + META_SIZE);
+    }
+
+    m61_stats_alloc(ptr, sz, file, line);
+
     return ptr;
 }
 
+static void m61_try_merge_mem(uintptr_t ptr, size_t aligned_size)
+{
+    auto [new_it, inserted] = map_freed_size.insert({ptr, aligned_size});
+    assert(inserted);
+
+    // 向后合并
+    auto it = std::next(new_it);
+    if (it != map_freed_size.end())
+    {
+        if (ptr + aligned_size == it->first)
+        {
+            new_it->second += it->second;
+            map_freed_size.erase(it);
+        }
+    }
+
+    // 向前合并
+    if (new_it != map_freed_size.begin())
+    {
+        it = std::prev(new_it);
+        if (it->first + it->second == ptr)
+        {
+            it->second += new_it->second;
+            map_freed_size.erase(new_it);
+        }
+    }
+}
 
 /// m61_free(ptr, file, line)
 ///    Frees the memory allocation pointed to by `ptr`. If `ptr == nullptr`,
@@ -65,9 +168,67 @@ void* m61_malloc(size_t sz, const char* file, int line) {
 ///    `file`:`line`.
 
 void m61_free(void* ptr, const char* file, int line) {
-    // avoid uninitialized variable warnings
-    (void) ptr, (void) file, (void) line;
+    if (!ptr)
+    {
+        return;
+    }
+
+    if ((uintptr_t)ptr < m61_get_statistics().heap_min || (uintptr_t)ptr >= m61_get_statistics().heap_max)
+    {
+        fprintf(stderr, "MEMORY BUG: %s:%d: invalid free of pointer %p, not in heap\n", file, line, ptr);
+        abort();
+    }
+
     // Your code here. The handout code does nothing!
+    auto it = map_alloced_size.find((uintptr_t)ptr);
+    if (it != map_alloced_size.end())
+    {
+        size_t sz = it->second - META_SIZE;
+        stats.nactive -= 1;
+        stats.active_size -= sz;
+
+        // 合并小块内存
+        m61_try_merge_mem(it->first, ROUND_UP(it->second));
+
+        // 检测是否有超出内存边界的写入
+        if (((char*)ptr)[sz] != CANARY)
+        {
+            fprintf(stderr, "MEMORY BUG: %s:%d: detected wild write during free of pointer %p\n", file, line, ptr);
+            abort();
+        }
+
+        // 删除活跃内存记录
+        map_alloced_size.erase(it);
+
+        // 记录删除记录
+        unset_freed.insert((uintptr_t)ptr);
+    }
+    else
+    {
+        if (unset_freed.count((uintptr_t)ptr))
+        {
+            fprintf(stderr, "MEMORY BUG: %s:%d: invalid free of pointer %p, double free\n", file, line, ptr);
+        }
+        else
+        {
+            fprintf(stderr, "MEMORY BUG: %s:%d: invalid free of pointer %p, not allocated\n", file, line, ptr);
+            if (!map_alloced_size.empty())
+            {
+                auto it_lower_bound = map_alloced_size.lower_bound((uintptr_t)ptr);
+                if (it_lower_bound != map_alloced_size.begin()){
+                    it_lower_bound = std::prev(it_lower_bound);
+
+                    if (it_lower_bound->first + it_lower_bound->second >= (uintptr_t)ptr)
+                    {
+                        m61_memory_meta* meta = reinterpret_cast<m61_memory_meta*>((char*)it_lower_bound->first + it_lower_bound->second - META_SIZE + CANARY_SIZE);
+                        fprintf(stderr, "  %s:%d: %p is %lu bytes inside a %lu byte region allocated here\n", meta->file, meta->line, ptr, (uintptr_t)ptr - it_lower_bound->first, it_lower_bound->second - META_SIZE);
+                    }
+                }
+            }
+        }
+
+        abort();
+    }
 }
 
 
@@ -80,6 +241,17 @@ void m61_free(void* ptr, const char* file, int line) {
 
 void* m61_calloc(size_t count, size_t sz, const char* file, int line) {
     // Your code here (not needed for first tests).
+    if (count == 0 || sz == 0)
+    {
+        return nullptr;
+    }
+
+    if ((size_t) -1 / sz < count || (size_t) -1 / count < sz)
+    {
+        m61_stats_alloc(nullptr, count * sz, file, line);
+        return nullptr;
+    }
+
     void* ptr = m61_malloc(count * sz, file, line);
     if (ptr) {
         memset(ptr, 0, count * sz);
@@ -94,8 +266,11 @@ void* m61_calloc(size_t count, size_t sz, const char* file, int line) {
 m61_statistics m61_get_statistics() {
     // Your code here.
     // The handout code sets all statistics to enormous numbers.
-    m61_statistics stats;
-    memset(&stats, 255, sizeof(m61_statistics));
+    if (stats.heap_min == 0)
+    {
+        stats.heap_min = (uintptr_t)default_buffer.buffer;
+        stats.heap_max = (uintptr_t)default_buffer.buffer + default_buffer.size;
+    }
     return stats;
 }
 
@@ -104,11 +279,11 @@ m61_statistics m61_get_statistics() {
 ///    Prints the current memory statistics.
 
 void m61_print_statistics() {
-    m61_statistics stats = m61_get_statistics();
+    m61_statistics local_stats = m61_get_statistics();
     printf("alloc count: active %10llu   total %10llu   fail %10llu\n",
-           stats.nactive, stats.ntotal, stats.nfail);
+           local_stats.nactive, local_stats.ntotal, local_stats.nfail);
     printf("alloc size:  active %10llu   total %10llu   fail %10llu\n",
-           stats.active_size, stats.total_size, stats.fail_size);
+           local_stats.active_size, local_stats.total_size, local_stats.fail_size);
 }
 
 
@@ -118,4 +293,10 @@ void m61_print_statistics() {
 
 void m61_print_leak_report() {
     // Your code here.
+    for (auto& alloced : map_alloced_size)
+    {
+        size_t sz = alloced.second - META_SIZE;
+        m61_memory_meta* meta = reinterpret_cast<m61_memory_meta*>((char*)alloced.first + sz + CANARY_SIZE);
+        printf("LEAK CHECK: %s:%d: allocated object %p with size %lu\n", meta->file, meta->line, (void*)alloced.first, sz);
+    }
 }
